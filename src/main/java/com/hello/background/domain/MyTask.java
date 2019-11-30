0772afeafe5c94b7b1a2edc1c3ba610f1bf90da4
wuketao <wuketao@houbank.com>
2019-11-30 09:07:04 +0800

    添加注解

--- src/main/java/com/hello/background/domain/MyTask.java
+++ src/main/java/com/hello/background/domain/MyTask.java
@@ -1,10 +1,22 @@
 package com.hello.background.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.Entity;
+
 /**
  * 我的任务
  * @author wuketao
  * @date 2019/11/30
  * @Description
  */
+@Slf4j
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class MyTask {
 }
