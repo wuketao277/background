f818a268826b346146db7a8b4df8a18d4090e108
wuketao <wuketao@houbank.com>
2019-11-30 08:47:04 +0800

    增加客户实体类

---
+++ src/main/java/com/hello/background/domain/Customer.java
@@ -0,0 +1,22 @@
+package com.hello.background.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.Entity;
+
+/**
+ * 客户
+ * @author wuketao
+ * @date 2019/11/30
+ * @Description
+ */
+@Slf4j
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Customer {
+}
