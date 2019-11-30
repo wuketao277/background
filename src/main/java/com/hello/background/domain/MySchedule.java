2961f35c7ec3f90d1eff5eb0a0d7ff3fbf6ee72a
wuketao <wuketao@houbank.com>
2019-11-30 09:44:49 +0800

    增加属性

--- src/main/java/com/hello/background/domain/MySchedule.java
+++ src/main/java/com/hello/background/domain/MySchedule.java
@@ -5,7 +5,11 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
 
 /**
  * 我的计划
@@ -19,4 +23,35 @@ import javax.persistence.Entity;
 @AllArgsConstructor
 @Entity
 public class MySchedule {
+
+    /**
+     * 计划主键id
+     */
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    /**
+     * 计划的标题
+     */
+    @Column(length = 200, nullable = false)
+    private String scheduleTitle;
+
+    /**
+     * 计划的内容
+     */
+    @Column(length = 2000, nullable = false)
+    private String scheduleContent;
+
+    /**
+     * 计划的创建人ID
+     */
+    @Column(length = 50, nullable = false)
+    private String scheduleUserId;
+
+    /**
+     * 计划的时间
+     */
+    @Column(nullable = false)
+    private LocalDateTime scheduleDateTime;
 }
