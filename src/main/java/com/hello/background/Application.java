3b3f561335f23bfd6ce6dac9aff40bc3e5eae26c
wuketao <wuketao@houbank.com>
2021-02-08 18:48:08 +0800

    取消redis session

--- src/main/java/com/hello/background/Application.java
+++ src/main/java/com/hello/background/Application.java
@@ -2,7 +2,6 @@ package com.hello.background;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -12,7 +11,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @EnableSwagger2
 @SpringBootApplication
-@EnableRedisHttpSession
+//@EnableRedisHttpSession
 public class Application {
     public static void main(String[] args){
         SpringApplication.run(Application.class,args);
