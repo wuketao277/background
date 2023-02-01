b163db14ae9a338cc8d8a1c54d06c98f2d7f720e
wuketao277 <howard.wu@helloapplicant.com>
2023-02-01 16:45:41 +0800

    优化

--- src/main/java/com/hello/background/service/CommentService.java
+++ src/main/java/com/hello/background/service/CommentService.java
@@ -192,7 +192,7 @@ public class CommentService {
                 candidateOptional.ifPresent(x -> candidateMap.put(x.getId(), x));
             }
             if (candidateMap.containsKey(vo.getCandidateId())) {
-                vo.setClientName(candidateMap.get(vo.getCandidateId()).getChineseName());
+                vo.setCandidateName(candidateMap.get(vo.getCandidateId()).getChineseName());
             }
             commentVOList.add(vo);
         }
