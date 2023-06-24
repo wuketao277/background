eb693fea7373e9557eb12b9180e5f2db47214858
wuketao277 <howard.wu@helloapplicant.com>
2023-06-24 09:31:50 +0800

    kpi工作天数调整功能

--- src/main/java/com/hello/background/controller/KPIWorkDaysAdjustController.java
+++ src/main/java/com/hello/background/controller/KPIWorkDaysAdjustController.java
@@ -3,10 +3,13 @@ package com.hello.background.controller;
 import com.hello.background.service.KPIWorkDaysAdjustService;
 import com.hello.background.vo.KPIWorkDaysAdjustRequest;
 import com.hello.background.vo.KPIWorkDaysAdjustVO;
+import com.hello.background.vo.UserVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * @author wuketao
@@ -47,8 +50,9 @@ public class KPIWorkDaysAdjustController {
      * @return
      */
     @PostMapping("queryPage")
-    public Page<KPIWorkDaysAdjustVO> queryPage(@RequestBody KPIWorkDaysAdjustRequest request) {
-        return service.queryPage(request.getSearch(), request.getCurrentPage(), request.getPageSize());
+    public Page<KPIWorkDaysAdjustVO> queryPage(@RequestBody KPIWorkDaysAdjustRequest request, HttpSession session) {
+        UserVO userVO = (UserVO) session.getAttribute("user");
+        return service.queryPage(request.getSearch(), request.getCurrentPage(), request.getPageSize(), userVO);
     }
 
     /**
--- src/main/java/com/hello/background/service/KPIWorkDaysAdjustService.java
+++ src/main/java/com/hello/background/service/KPIWorkDaysAdjustService.java
@@ -1,14 +1,23 @@
 package com.hello.background.service;
 
+import com.hello.background.constant.RoleEnum;
 import com.hello.background.domain.KPIWorkDaysAdjust;
 import com.hello.background.repository.KPIWorkDaysAdjustRepository;
 import com.hello.background.utils.TransferUtil;
 import com.hello.background.vo.KPIWorkDaysAdjustVO;
+import com.hello.background.vo.UserVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.*;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -51,10 +60,23 @@ public class KPIWorkDaysAdjustService {
      * @param pageSize
      * @return
      */
-    public Page<KPIWorkDaysAdjustVO> queryPage(String search, Integer currentPage, Integer pageSize) {
+    public Page<KPIWorkDaysAdjustVO> queryPage(String search, Integer currentPage, Integer pageSize, UserVO user) {
         Sort sort = new Sort(Sort.Direction.DESC, "id");
         Pageable pageable = new PageRequest(currentPage - 1, pageSize, sort);
-        Page<KPIWorkDaysAdjust> all = repository.findAll(pageable);
+        // 设置查询条件
+        Specification<KPIWorkDaysAdjust> specification = new Specification<KPIWorkDaysAdjust>() {
+            @Override
+            public Predicate toPredicate(Root<KPIWorkDaysAdjust> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
+                List<Predicate> list = new ArrayList<>();
+                if (!user.getRoles().contains(RoleEnum.ADMIN) && !user.getRoles().contains(RoleEnum.ADMIN_COMPANY)) {
+                    // 普通用户只能查询自己的信息
+                    list.add(criteriaBuilder.equal(root.get("userName"), user.getUsername()));
+                }
+                Predicate[] p = new Predicate[list.size()];
+                return criteriaBuilder.and(list.toArray(p));
+            }
+        };
+        Page<KPIWorkDaysAdjust> all = repository.findAll(specification, pageable);
         Page<KPIWorkDaysAdjustVO> map = all.map(x -> TransferUtil.transferTo(x, KPIWorkDaysAdjustVO.class));
         map = new PageImpl<>(map.getContent(),
                 new PageRequest(map.getPageable().getPageNumber(), map.getPageable().getPageSize()),
