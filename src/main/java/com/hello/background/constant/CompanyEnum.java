7f9a947b375ae898bec90385e32710573d5ad370
wuketao277 <howard.wu@helloapplicant.com>
2023-07-04 14:02:09 +0800

    优化

--- src/main/java/com/hello/background/constant/CompanyEnum.java
+++ src/main/java/com/hello/background/constant/CompanyEnum.java
@@ -12,7 +12,7 @@ public enum CompanyEnum {
     Shanghaihailuorencaikeji("上海海罗人才科技有限公司"),
     Shenyanghailuorencaifuwu("沈阳海罗人才服务有限公司"),
     Nanjinghailuorencaifuwu("南京海罗人才服务有限公司"),
-    Wuhanhailuorencaifuwu("武汉海罗人才服务有限公司");
+    Wuhanhailuorencaifuwu("武汉海罗河山人才服务有限公司");
 
     private String name;
 
