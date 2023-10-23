414c88e49f6e6653596540d0c56718885e2c448a
wuketao277 <howard.wu@helloapplicant.com>
2023-10-23 20:36:02 +0800

    优化

--- src/main/java/com/hello/background/controller/SecurityController.java
+++ src/main/java/com/hello/background/controller/SecurityController.java
@@ -104,6 +104,6 @@ public class SecurityController {
      */
     @RequestMapping("checkVersion")
     public boolean checkVersion(String version) {
-        return "1.0.9".equals(version);
+        return "1.1.0".equals(version);
     }
 }
